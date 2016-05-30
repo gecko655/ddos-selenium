2dafeef9522157dd75a6090db842a15a6cb523a3
gecko655 <aqwsedrft1234@yahoo.co.jp>
2016-05-31 00:19:03 +0900

    Initial commit

---
+++ main.rb
@@ -0,0 +1,21 @@
+require "selenium-webdriver"
+require 'logger'
+
+logger = Logger.new(STDOUT)
+
+hostname = ENV["Hostserver_Hostname"]
+raise ArgumentError "usage: ruby #{$0} url" unless ARGV[0]
+url = $0
+
+logger.info("Initializing")
+driver = Selenium::WebDriver.for :remote, :url => "http://"+hostname+":4444/wd/hub", :desired_capabilities => :chrome
+begin
+  driver.manage.timeouts.implicit_wait = 10
+
+  logger.info("Accessing to the page: "+url)
+  driver.navigate.to url
+  logger.info("done")
+ensure 
+  sleep 3 
+  driver.quit
+end
---
+++ run.sh
@@ -0,0 +1,2 @@
+docker run -d --name ddos-selenium -e Hostserver_Hostname=`hostname` gecko655/ddos-selenium
+
